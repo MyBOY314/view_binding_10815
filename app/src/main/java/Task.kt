fe68aa0eb87fc02e6645b5132ea98f1297f019ba
Dave <davehabelpaprindey@gmail.com>
2022-09-13 08:28:23 +0700

    GD4 View Binding

---
+++ app/src/main/java/Task.kt
@@ -0,0 +1,1 @@
+data class Task (val chapter : String, val title : String, val details : String)
---
+++ app/src/main/java/TaskList.kt
@@ -0,0 +1,9 @@
+object TaskList {
+    val TaskList = listOf<Task>(
+        Task("Chapter 1","Bangun Tidur Lagi...",
+            "Buka mata lagi..."),
+        Task("Chapter 2","Bury The Light","Standing Here I Realize"),
+        Task("Chapter 3","There will be BLOODSHED","And yet why are we still here?"),
+        Task("Chapter 4","I told you to forget me","But you stayed by my side")
+    )
+}
